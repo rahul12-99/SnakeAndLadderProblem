266c35b39bb7946b441243e04e0b2a8660b4dc17
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-28 19:31:54 +0530

    Added: Code for player check a option to play

--- src/com/snakeandladder/SnakeAndLadderSimulator.java
+++ src/com/snakeandladder/SnakeAndLadderSimulator.java
@@ -2,7 +2,25 @@ package com.snakeandladder;
 
 public class SnakeAndLadderSimulator {
     public static void main(String[] args) {
-        int x = (int) Math.floor(Math.random()*(6 -1 +1) + 1);
-        System.out.println("The player rolled the dice and get a number : " + x);
+        int playerposition = 0;
+        // to get the random number 1 to 6
+        int getnumber = (int) Math.floor(Math.random()*(6 -1 +1) + 1);
+        // to get the random option
+        int getoption = (int) Math.floor(Math.random()*(3 -1 +1) + 1);
+        switch (getoption){
+            // case of no play:
+            case 0:
+                playerposition = playerposition;
+                break;
+            //case of ladder:
+            case 1:
+                playerposition += getnumber;
+                break;
+            // case of snake:
+            case 2:
+                playerposition -= getnumber;
+                break;
+        }
+        System.out.println(playerposition);
     }
 }
