c67cb3d257d1d538cea71847052e973fc0c8f517
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-28 19:37:47 +0530

    Added: Code for player to reach winning position

--- src/com/snakeandladder/SnakeAndLadderSimulator.java
+++ src/com/snakeandladder/SnakeAndLadderSimulator.java
@@ -3,23 +3,24 @@ package com.snakeandladder;
 public class SnakeAndLadderSimulator {
     public static void main(String[] args) {
         int playerposition = 0;
-        // to get the random number 1 to 6
-        int getnumber = (int) Math.floor(Math.random()*(6 -1 +1) + 1);
-        // to get the random option
-        int getoption = (int) Math.floor(Math.random()*(3 -1 +1) + 1);
-        switch (getoption){
-            // case of no play:
-            case 0:
-                playerposition = playerposition;
-                break;
-            //case of ladder:
-            case 1:
-                playerposition += getnumber;
-                break;
-            // case of snake:
-            case 2:
-                playerposition -= getnumber;
-                break;
+        while (playerposition < 100) {
+            int getnumber = (int) Math.floor(Math.random() * (6 - 1 + 1) + 1);
+            int getoption = (int) Math.floor(Math.random() * (3 - 1 + 1) + 1);
+            switch (getoption) {
+                case 0:
+                    playerposition = playerposition;
+                    break;
+                case 1:
+                    playerposition += getnumber;
+                    break;
+                case 2:
+                    playerposition -= getnumber;
+                    break;
+
+            }
+            if (playerposition < 0){
+                playerposition = 0;
+            }
         }
         System.out.println(playerposition);
     }
