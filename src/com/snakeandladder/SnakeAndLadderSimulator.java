de42c1b6624c58462c2f2b6fd8d7565caa2710a1
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-28 19:15:49 +0530

    Added: Welcome message

---
+++ src/com/snakeandladder/SnakeAndLadderSimulator.java
@@ -0,0 +1,7 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderSimulator {
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder problem");
+    }
+}
